880564ee971e01532c86a3b984a7efb943a0d45f
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 10:42:50 -0600

    Rewriting as Laminas Project package

--- src/Document/NodeList.php
+++ src/Document/NodeList.php
@@ -1,23 +1,22 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-dom for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-dom/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-dom/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Dom\Document;
+namespace Laminas\Dom\Document;
 
 use ArrayAccess;
 use Countable;
 use DOMNode;
 use DOMNodeList;
 use Iterator;
-use Zend\Dom\Exception;
+use Laminas\Dom\Exception;
 
 /**
- * DOMNodeList wrapper for Zend\Dom\Document\Query results
+ * DOMNodeList wrapper for Laminas\Dom\Document\Query results
  */
 class NodeList implements Iterator, Countable, ArrayAccess
 {
